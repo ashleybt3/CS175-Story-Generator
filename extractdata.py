abba5cc748fe38ee074d13c9a8e32a80bb7823df
Ashley Teves <ashleybtev@gmail.com>
2021-02-23 20:43:37 -0800

    story corpus

---
+++ extractdata.py
@@ -0,0 +1,19 @@
+from sklearn.model_selection import train_test_split
+
+with open('corpus.txt', 'r', encoding = 'utf-8') as file:
+	#strip trailing characters and white space
+	dataset = ["<|title|>" + line.rstrip() for line in file.readlines()]
+
+# split into training data and validation(test) data
+# train_size = proportion of dtaset to include in training
+# random_state = controls shuffling applied to data before split
+# https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+train, test = train_test_split(dataset, train_size = 0.9, random_state = 1000)
+
+print("training data size: ", len(train))
+print("validation data size: ", len(test))
+
+with open("train_temp.txt", "w") as file_handle:
+	file_handle.write("<|endoftext|>".join(train))
+with open("valid_temp.txt", "w") as file_handle:
+	file_handle.write("<|endoftext|>".join(test))
--- model2.py
+++ model2.py
@@ -1,20 +1,38 @@
 import tensorflow as tf
 import time
-from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+import torch
+# from transformers import TFGPT2LMHeadModel, GPT2Tokenizer, pipeline
+from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForCausalLM
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM,
+    LogitsProcessorList,
+    MinLengthLogitsProcessor,
+    TopKLogitsWarper,
+    TemperatureLogitsWarper,
+    BeamSearchScorer,
+)
 
 
 # source: https://huggingface.co/blog/how-to-generate
 
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2-medium")
 # add the EOS token as PAD token to avoid warnings
-model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
+# model = TFGPT2LMHeadModel.from_pretrained("gpt2-medium", pad_token_id=tokenizer.eos_token_id )
+# model = TFGPT2LMHeadModel.from_pretrained("./test-clm", from_pt=True)
+
+
+# from transformers import AutoTokenizer, AutoModelWithLMHead
+tokenizer = AutoTokenizer.from_pretrained("pranavpsv/gpt2-genre-story-generator")
+
+model = AutoModelForCausalLM.from_pretrained("pranavpsv/gpt2-genre-story-generator")
+
 user_input = 'Once upon a time'
 story = ''
 def run():
     run = True
     while(run):
         global user_input
-        
         user_input =  input("> ")
         if (user_input == "quit"):
             run = False
@@ -27,20 +45,18 @@ def exec_model():
     # encode context the generation is conditioned on
     global user_input
     start = time.time()
-    input_ids = tokenizer.encode(user_input, return_tensors='tf')
+
+    
+    
+    input_ids = tokenizer.encode(user_input, return_tensors='pt')
+
+    # input_ids = tokenizer(user, return_tensors="pt").input_ids
     # generate text until the output length (which includes the context length) reaches 50
     greedy_output = model.generate(input_ids, max_length=50)
 
-
-    # METHODS:
-    # - Greedy Search
-    # - Beam Search
-    # - Sampling
-    # - TopK Sampling
-    # - TopP(nucleus) Sampling
-
     # set seed to reproduce results. Feel free to change the seed though to get different results
     # tf.random.uniform([1], seed=1)
+    tf.random.set_seed(1)
 
     # set top_k = 50 and set top_p = 0.95 and num_return_sequences = 3
     sample_outputs = model.generate(
@@ -48,11 +64,13 @@ def exec_model():
         do_sample=True, 
         max_length=50, 
         # TopK sampling
-        top_k=50, 
+        top_k=40, 
         # TopP sampling
-        top_p=0.95, 
+        top_p=0.9, 
         # number of highest scoring beams
         num_return_sequences=3,
+        temperature = 0.7,
+        repetition_penalty=1.2,
 
         # Beam Search
         num_beams = 5,
@@ -69,13 +87,21 @@ def exec_model():
     
     output = tokenizer.decode(sample_outputs[0], skip_special_tokens=True)
     global story
-    user_input += output
     story = story + " " + output
     end = time.time()
     print(output)
+    
     print("time: ", end - start)
+    
 
 if __name__ == "__main__":
+
+    # nlp = pipeline("ner")
+    # sequence = "Hugging Face Inc. is a company based in New York City. Its headquarters are in DUMBO, therefore very" \
+    #        "close to the Manhattan Bridge which is visible from the window."
+
+    # print(nlp(sequence))
+
     print(100 * '-')
     exec_model()
     run()
