c9620db355d96447c619afcccb28da2a903bba06
Tiffany Kong <tiffy.wildstar@gmail.com>
2021-03-04 18:27:26 -0800

    include pos checker, context improves slightly, max token error encountered

--- transformer.py
+++ transformer.py
@@ -5,13 +5,18 @@ import numpy
 import random
 
 import language_tool_python
-
+import nltk
+from nltk.corpus import stopwords
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-model = TFGPT2LMHeadModel.from_pretrained("C:/Users/tiffany/Desktop/CS175-Story-Generator/output_clm", from_pt = True)
+model = TFGPT2LMHeadModel.from_pretrained("./output_clm", from_pt = True)
 
-user_input = "Once upon a time"
-story = ""
+key_words = set()
+stop_words = set(stopwords.words('english'))
+pos = ['NN', 'NNS', 'NNP', 'NNPS', 'PDT', 'POS', 'PRP', 'PRP$' ]
+
+user_input = "Long ago, "
+#story = ""
 
 def run():
 	run = True
@@ -23,13 +28,56 @@ def run():
 		user_input = user_input + " " + new_input
 		if (new_input == "quit"):
 		    run = False
-		elif (new_input == "story"):
-		    print(story)
-		    print()
+		# elif (new_input == "story"):
+		#     print(story)
+		#     print()
 		elif (new_input == "\n"):
 			print("invalid, please enter text\n")
 		else:
 		    exec_model()
+
+def part_of_speech(output):
+    wordsList = nltk.word_tokenize(output) 
+    wordsList = [w for w in wordsList if not w in stop_words]    
+    tagged = nltk.pos_tag(wordsList) 
+    for word, tag in tagged:
+        if tag in pos:
+            key_words.add(word.lower())
+
+def best_sequence(sample_outputs):
+    #print("Output:\n" + 100 * '-')
+    
+    total_words = 0
+    total_pos = 0
+    matched_words = 0
+    matched_pos = 0
+    best_output = None
+    backup_output = None
+    for i, sample_output in enumerate(sample_outputs):
+        output = tokenizer.decode(sample_output, skip_special_tokens=True)
+        wordsList = nltk.word_tokenize(output.lower()) 
+        wordsList = [w for w in wordsList if not w in stop_words]    
+        tagged = nltk.pos_tag(wordsList) 
+        for word, tag in tagged:
+            if word in key_words:
+                matched_words += 1
+            if tag in pos:
+                matched_pos += 1
+        if matched_words > total_words:
+            best_output = output
+        if matched_pos > total_pos:
+            backup_output = output
+        matched_pos = 0
+        matched_words = 0
+        #print("{}: {}".format(i, output))
+    if not best_output:
+        print("finding back up:", backup_output)
+        best_output = backup_output
+    print()
+    
+    # print(best_output)
+    return best_output
+
 
 def exec_model():
 	global user_input
@@ -73,22 +121,24 @@ def exec_model():
 
 	)
 
+	best_output = best_sequence(sample_outputs)
+	part_of_speech(best_output)
 	# prints the random sequence
-	next_phrase = random.choice(sample_outputs) #max(sample_outputs, key = len)
-	output = tokenizer.decode(next_phrase, skip_special_tokens=True)
-	global story
-	story = story + " " + output
+	#next_phrase = random.choice(sample_outputs) #max(sample_outputs, key = len)
+	#output = tokenizer.decode(best_output, skip_special_tokens=True)
+	#global story
+	#story = story + " " + best_output
 
 	#model cannot process too big of output, take only last 10 words
 	#average words in sentence = 15-20 (10 words from original output + (~5-10) words from user_input)
-	user_input = output#" ".join(output.split()[-10:]) 
+	user_input = best_output#" ".join(output.split()[-10:]) 
 	#print("updated user_input: ", user_input)
 	end = time.time()
 
 	print("\nthe story so far=========:")
-	matches = tool.check(output)
-	output = tool.correct(output)
-	print(output)
+	matches = tool.check(best_output)
+	best_output = tool.correct(best_output)
+	print(best_output)
 
 	print("time: {:.2f} seconds".format(end - start))
 	print()
