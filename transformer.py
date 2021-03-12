0236f4843c5b543081b85df7551b9e0c09d879e8
Tiffany Kong <tiffy.wildstar@gmail.com>
2021-03-11 16:59:25 -0800

    edit outlook of file and generated sample output

--- transformer.py
+++ transformer.py
@@ -15,23 +15,17 @@ key_words = set()
 stop_words = set(stopwords.words('english'))
 pos = ['NN', 'NNS', 'NNP', 'NNPS', 'PDT', 'POS', 'PRP', 'PRP$' ]
 
-user_input = "Long ago, "
-#story = ""
+user_input = "Once upon a time, "
 
 def run():
 	run = True
 	while(run):
 		global user_input
-		print('-----INSTRUCTIONS:-----\n * Enter any text to continue the story\n * Avoid closing sentence(".", "!", "?") if possible\n * If you must close a sentence, start a new sentence with a few words\n--------------------------\n')
-		#user_input =  input("> ")
-		new_input = input("> ")
-		user_input = user_input + " " + new_input
-		if (new_input == "quit"):
+		new_user_input = input("> ")
+		user_input = user_input + " " + new_user_input
+		if (new_user_input == "quit"):
 		    run = False
-		# elif (new_input == "story"):
-		#     print(story)
-		#     print()
-		elif (new_input == "\n"):
+		elif (new_user_input == "\n"):
 			print("invalid, please enter text\n")
 		else:
 		    exec_model()
@@ -44,9 +38,7 @@ def part_of_speech(output):
         if tag in pos:
             key_words.add(word.lower())
 
-def best_sequence(sample_outputs):
-    #print("Output:\n" + 100 * '-')
-    
+def best_sequence(sample_outputs):   
     total_words = 0
     total_pos = 0
     matched_words = 0
@@ -69,49 +61,29 @@ def best_sequence(sample_outputs):
             backup_output = output
         matched_pos = 0
         matched_words = 0
-        #print("{}: {}".format(i, output))
     if not best_output:
         print("finding back up:", backup_output)
         best_output = backup_output
     print()
     
-    # print(best_output)
     return best_output
 
 
 def exec_model():
 	global user_input
-	print("\n---generating output in progress---")
-	print("user_input: ", user_input)
+	print("\n========In process of generating output...========")
 	tool = language_tool_python.LanguageTool('en-US')
 
 	start = time.time()
 
 	input_ids = tokenizer.encode(user_input, return_tensors='tf')
 
-	# input_ids = tokenizer(user, return_tensors="pt").input_ids
-	# generate text until the output length (which includes the context length) reaches 50
-	#greedy_output = model.generate(input_ids, max_length=len(user_input))
-
-	# set seed to reproduce results. Feel free to change the seed though to get different results
-	# tf.random.uniform([1], seed=1)
 	tf.random.set_seed(0)
 
 	sample_outputs = model.generate(
 		input_ids,
-		# max_length=100,  
-		# min_length = 100,
-		# num_return_sequences=10,
-		# no_repeat_ngram_size=2,
-		# repetition_penalty=1.5,
-		# top_p=0.92,
-		# temperature=.85,
-		# do_sample=True,
-		# top_k=125,
-		# early_stopping=True,
-
 		do_sample = True,
-		max_length = 10000,
+		max_length = 10000, #prevent overload of input
 		min_length = 50,
 		top_p = 0.92,
 		top_k = 125,
@@ -123,32 +95,22 @@ def exec_model():
 	)
 
 	best_output = best_sequence(sample_outputs)
-	part_of_speech(best_output)
-	# prints the random sequence
-	#next_phrase = random.choice(sample_outputs) #max(sample_outputs, key = len)
-	#output = tokenizer.decode(best_output, skip_special_tokens=True)
-	#global story
-	#story = story + " " + best_output
+	part_of_speech(best_output)	
 
-	#model cannot process too big of output, take only last 10 words
-	#average words in sentence = 15-20 (10 words from original output + (~5-10) words from user_input)
-	#user_input = best_output#" ".join(output.split()[-10:]) 
-	#print("updated user_input: ", user_input)
-	
-
-	print("\nthe story so far=========:")
+	print("\n========Story Output:========:")
 	matches = tool.check(best_output)
 	best_output = tool.correct(best_output)
 	print(best_output)
 	user_input = best_output
-	
+
 	end = time.time()
-	print("time: {:.2f} seconds".format(end - start))
+	print("time to generate text: {:.2f} seconds".format(end - start))
 	print()
 
 if __name__ == "__main__":
 
 	print(100 * "-")
-
+	print('-----INSTRUCTIONS:-----\n * Enter any text after the ">" prompt to continue the story\n * Avoid closing sentence(".", "!", "?") if possible\n * If you must close a sentence, start a new sentence with a few words\n * To exit the process, please enter "quit" at the next input\n-----------------------\n')
+	
 	exec_model()
 	run()
